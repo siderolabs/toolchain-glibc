81b7dad144efa57502dc04091c40946ee5852e68
Andrew Rynhard <andrew@andrewrynhard.com>
2018-12-12 11:40:41 -0800

    feat: use relative symlinks

--- hack/fhs.sh
+++ hack/fhs.sh
@@ -8,25 +8,25 @@ rm -rf /bin
 mkdir -pv ${PREFIX}/{dev,etc,lib,opt,proc,sys}
 
 mkdir -pv ${PREFIX}/bin
-ln -sv /bin $PREFIX/sbin
+ln -sv bin $PREFIX/sbin
 # Required by glibc (looks for interpreter here)
-ln -sv /lib $PREFIX/lib64
+ln -sv lib $PREFIX/lib64
 
 mkdir -pv ${PREFIX}/usr/{include,}
-ln -sv /bin ${PREFIX}/usr/bin
-ln -sv /bin ${PREFIX}/usr/sbin
-ln -sv /lib ${PREFIX}/usr/lib
-ln -sv /var ${PREFIX}/usr/var
+ln -sv ../bin ${PREFIX}/usr/bin
+ln -sv ../bin ${PREFIX}/usr/sbin
+ln -sv ../lib ${PREFIX}/usr/lib
+ln -sv ../var ${PREFIX}/usr/var
 
 mkdir -pv ${PREFIX}/usr/local
-ln -sv /bin ${PREFIX}/usr/local/bin
-ln -sv /bin ${PREFIX}/usr/local/sbin
-ln -sv /lib ${PREFIX}/usr/local/lib
-ln -sv /usr/include ${PREFIX}/usr/local/include
+ln -sv ../../bin ${PREFIX}/usr/local/bin
+ln -sv ../../bin ${PREFIX}/usr/local/sbin
+ln -sv ../../lib ${PREFIX}/usr/local/lib
+ln -sv ../../usr/include ${PREFIX}/usr/local/include
 
 mkdir -pv ${PREFIX}/run
 mkdir -pv ${PREFIX}/var/{log,mail,spool}
-ln -sv /run $PREFIX/var/run
+ln -sv ../run $PREFIX/var/run
 
 install -dv -m 0750 ${PREFIX}/root
 install -dv -m 1777 ${PREFIX}/tmp ${PREFIX}/var/tmp
